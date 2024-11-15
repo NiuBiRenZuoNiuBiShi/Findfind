06c2d9204cbc47562a67531b1cf004a04764f301
fankeqing <1377426063@qq.com>
2024-11-15 11:20:43 +0800

    aaa

--- src/main/java/com/bugvictims/demo11/Controller/UserController.java
+++ src/main/java/com/bugvictims/demo11/Controller/UserController.java
@@ -1,4 +1,8 @@
 package com.bugvictims.demo11.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class UserController {
+
 }
