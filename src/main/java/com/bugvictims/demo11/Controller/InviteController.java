bb10ae22b40be9d3c484a4feb37827976ef43f37
86138 <zy1772696711@gmail.com>
2024-12-27 15:15:33 +0800

    .gitignore_fixed && libs

--- src/main/java/com/bugvictims/demo11/Controller/InviteController.java
+++ src/main/java/com/bugvictims/demo11/Controller/InviteController.java
@@ -1,15 +1,6 @@
 package com.bugvictims.demo11.Controller;
-import com.bugvictims.demo11.Service.InviteRequestService;
-import com.bugvictims.demo11.Service.TeamService;
-import com.bugvictims.demo11.Service.TeamUserService;
-import com.bugvictims.demo11.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
-import com.bugvictims.demo11.Pojo.*;
-@RestController
-@RequestMapping("/invite")
+
+
 public class InviteController {
 
 }
